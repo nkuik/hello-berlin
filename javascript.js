18129b34aa6349643fdd28d2c0bf57619d4100d6
Nathan Kuik <nathan.kuik@gmail.com>
2017-02-12 17:30:35 +0100

    Copy over from previous site

---
+++ javascript.js
@@ -0,0 +1,9 @@
+window.sr = ScrollReveal();
+sr.reveal('.welcome', { duration: 1000});
+sr.reveal('.description-detail', { duration: 1000}, 300);
+sr.reveal('.question', { duration: 1000});
+sr.reveal('.path', { duration: 200}, 300);
+sr.reveal('#cd-timeline', { duration: 100, viewFactor: 0.13 });
+sr.reveal('.cd-timeline-block', { duration: 100, viewFactor: 0.5 });
+sr.reveal('.project', { duration: 200 });
+sr.reveal('.icons', { duration: 500}, 100);
